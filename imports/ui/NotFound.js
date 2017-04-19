2b5edf1bd6fa5f52572cf3d905eafbc773742bde
Alex Trindade <trindadealex@gmail.com>
2017-04-19 18:04:05 -0300

    Update Not Found page

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -6,7 +6,7 @@ export default class NotFound extends React.Component {
     return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>Page Not Found</h1>
+          <h1>404 - Page Not Found</h1>
           <p>Humm, you are unable to find that page.</p>
           <Link to="/" className="button button--link">HEAD HOME</Link>
         </div>
